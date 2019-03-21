7b7a72f72bc03a8e53da617586ebe95f654f308d
dheeraj <dheeraj.dubey@ascenaretail.com>
2019-03-21 14:54:28 +0530

    ham java changed

--- hello/src/com/java/training/Ham.java
+++ hello/src/com/java/training/Ham.java
@@ -4,7 +4,7 @@ public class Ham {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hi");
+		System.out.println("hi my name is kumar");
 	}
 
 }
